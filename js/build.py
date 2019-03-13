145d1482b50aa14ee30900c91c6a75fd4bbd8226
Samuel Colvin <s@muelcolvin.com>
2019-03-13 19:18:30 +0000

    switching to iframe files

--- js/build.py
+++ js/build.py
@@ -13,99 +13,110 @@ from pathlib import Path
 
 THIS_DIR = Path(__file__).parent
 
+main_domain = os.getenv('REACT_APP_DOMAIN')
+if not main_domain:
+    print('WARNING: "REACT_APP_DOMAIN" env var not set')
+    main_domain = 'example.com'
 
-CSP = {
+main_csp = {
     'default-src': [
         "'self'",
     ],
     'script-src': [
         "'self'",
-        'www.google-analytics.com',
         'storage.googleapis.com',  # workbox, TODO remove and change CDN
-        '*.google.com',
     ],
     'font-src': [
         "'self'",
         'data:',
-        'fonts.gstatic.com',
     ],
     'style-src': [
         "'self'",
-        "'unsafe-inline'",
+        "'unsafe-inline'",  # TODO remove
     ],
     'frame-src': [
+        "'self'",
         'data:',
     ],
     'img-src': [
         "'self'",
         'blob:',
         'data:',
-        'www.google-analytics.com',
-        '*.googleapis.com',
-        '*.gstatic.com',
-        '*.google.com',
-        '*.google.co.uk',
-        '*.google.de',
-        '*.google.pt',
     ],
     'media-src': [
         "'self'",
     ],
     'connect-src': [
         "'self'",
-        '*.google-analytics.com',
         'https://sentry.io',
+        f'ui.{main_domain}',
+        f'auth.{main_domain}'
+    ],
+}
+
+auth_iframe_csp = {
+    'default-src': [
+        "'none'",
+    ],
+    'connect-src': [
+        f'auth.{main_domain}',
+    ],
+    'style-src': [
+        f'app.{main_domain}',
     ],
 }
 
 
-toml_template = """
-[[headers]]
-  for = "/*"
-  [headers.values]
-    Content-Security-Policy = "{}"
-"""
-
-
 def before():
+    # remove the unused reload prompt stuff from index.html
     path = THIS_DIR / 'src' / 'index.js'
     new_content = re.sub(r'^// *{{.+?^// *}}', '', path.read_text(), flags=re.S | re.M)
     new_content = re.sub(r'\n+$', '\n', new_content)
     path.write_text(new_content)
 
+    # TODO replace bootstrap with real thing
+    # TODO replace urls in THIS_DIR / 'build' / 'auth-iframes' / 'login.html'
 
-def after():
-    csp = dict(CSP)
 
-    main_domain = os.getenv('REACT_APP_DOMAIN')
-    if main_domain:
-        csp['connect-src'] += [f'ui.{main_domain}', f'auth.{main_domain}']
-    else:
-        print('WARNING: "REACT_APP_DOMAIN" env var not set')
-
-    content = (THIS_DIR / 'build' / 'index.html').read_text()
+def get_script(path: Path):
+    content = path.read_text()
     m = re.search(r'<script>(.+?)</script>', content, flags=re.S)
     if m:
         js = m.group(1)
-        s = f"'sha256-{base64.b64encode(hashlib.sha256(js.encode()).digest()).decode()}'"
-        csp['script-src'].append(s)
+        return f"'sha256-{base64.b64encode(hashlib.sha256(js.encode()).digest()).decode()}'"
     else:
-        print('WARNING: runtime script now found in index.html')
+        print('WARNING: script now found in', path)
+
+
+def after():
+    index_script_src = get_script(THIS_DIR / 'build' / 'index.html')
+    if index_script_src:
+        main_csp['script-src'].append(index_script_src)
 
     raven_dsn = os.getenv('RAVEN_DSN', None)
     if raven_dsn:
         m = re.search(r'^https://(.+)@sentry\.io/(.+)', raven_dsn)
         if m:
             key, app = m.groups()
-            csp['report-uri'] = [f'https://sentry.io/api/{app}/security/?sentry_key={key}']
+            main_csp['report-uri'] = [f'https://sentry.io/api/{app}/security/?sentry_key={key}']
         else:
             print('WARNING: app and key not found in RAVEN_DSN', raven_dsn)
-    csp = ' '.join(f'{k} {" ".join(v)};' for k, v in csp.items())
-    print('setting CSP header:', csp)
-    extra = toml_template.format(csp)
+
+    login_script_src = get_script(THIS_DIR / 'build' / 'auth-iframes' / 'login.html')
+    if login_script_src:
+        auth_iframe_csp['script-src'] = [login_script_src]
+
+    replacements = {
+        'main_csp': main_csp,
+        'auth_iframe_csp': auth_iframe_csp,
+    }
     path = THIS_DIR / '..' / 'netlify.toml'
-    with path.open('a') as f:
-        f.write(extra)
+    content = path.read_text()
+    for k, v in replacements.items():
+        csp = ' '.join(f'{k} {" ".join(v)};' for k, v in v.items())
+        print(f'setting {k} CSP header to: {csp}')
+        content = content.replace('{%s}' % k, csp)
+    path.write_text(content)
 
 
 if __name__ == '__main__':
